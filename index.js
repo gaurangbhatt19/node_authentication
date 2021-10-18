8c60d1b36c84ee15e4f4cd625429b1c51d8022c9
GBSourceCodes <gaurangbhatt19@gmail.com>
2021-10-19 02:04:53 +0530

    bcrypt

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express=require('express')
 const app = express()
+const bcrypt=require("bcrypt")
 app.use(express.json())
+
 var users=[]
+
 app.get("/users",(req,res)=>{
 let users=[{name:"Gaurang"}]
     res.json(users);
 })
 
-app.post("/users",(req,res)=>{
-    let user={name:req.body.name,password:req.body.password}
-    users.push(user);
-    res.send(users)
+app.post("/users",async(req,res)=>{
+    try{
+
+        const salt =bcrypt.genSalt(20);
+        const hashPassword=await bcrypt.hash(req.body.password,salt); 
+        const user={
+            name:req.body.name,
+            password: hashPassword
+        }
+        console.log(hashPassword)
+        
+        users.push(user);
+        res.status(201).send(users)
+    }catch(e){
+        res.status(500).send()
+    }
+   
 })
 
 
